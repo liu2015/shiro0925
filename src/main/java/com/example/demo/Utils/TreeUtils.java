e85f05d6b4908186f5704b75c20d0504b97613bc
ZHY <liufuling14@163.com>
2019-09-29 16:02:24 +0800

    计划先写添加用户,修改用户部分代码实现

--- src/main/java/com/example/demo/Utils/TreeUtils.java
+++ src/main/java/com/example/demo/Utils/TreeUtils.java
@@ -9,17 +9,14 @@ import java.util.List;
 public class TreeUtils {
 
 
-    public static List<SysMenu> getChildPerms(List<SysMenu> list, int parentId)
-    {
-        List<SysMenu> returnList = new ArrayList<SysMenu>();
-        for (Iterator<SysMenu> iterator = list.iterator(); iterator.hasNext();)
-        {
-            SysMenu t = (SysMenu) iterator.next();
+    public static List<SysMenu> getChildPerms(List<SysMenu> list, int parentId) {
+        List<SysMenu> returnList=new ArrayList<SysMenu>();
+        for (Iterator<SysMenu> iterator=list.iterator(); iterator.hasNext(); ) {
+            SysMenu t=(SysMenu) iterator.next();
             // 一、根据传入的某个父节点ID,遍历该父节点的所有子节点
-            if (t.getParentId() == parentId)
-            {
-                recursionFn(list, t);
-                returnList.add(t);
+            if (t.getParentId() == parentId) {
+                recursionFn( list, t );
+                returnList.add( t );
             }
         }
         return returnList;
@@ -32,40 +29,34 @@ public class TreeUtils {
      * @param list
      * @param t
      */
-    private static void recursionFn(List<SysMenu> list, SysMenu t)
-    {
+    private static void recursionFn(List<SysMenu> list, SysMenu t) {
         // 得到子节点列表
-        List<SysMenu> childList = getChildList(list, t);
-        t.setChildren(childList);
-        for (SysMenu tChild : childList)
-        {
-            if (hasChild(list, tChild))
-            {
+        List<SysMenu> childList=getChildList( list, t );
+        t.setChildren( childList );
+        for (SysMenu tChild : childList) {
+            if (hasChild( list, tChild )) {
                 // 判断是否有子节点
-                Iterator<SysMenu> it = childList.iterator();
-                while (it.hasNext())
-                {
-                    SysMenu n = (SysMenu) it.next();
-                    recursionFn(list, n);
+                Iterator<SysMenu> it=childList.iterator();
+                while (it.hasNext()) {
+                    SysMenu n=(SysMenu) it.next();
+                    recursionFn( list, n );
                 }
             }
         }
     }
 
 
-
     //    获得子节点
-    private static List<SysMenu> getChildList(List<SysMenu> list12 ,SysMenu tlist){
+    private static List<SysMenu> getChildList(List<SysMenu> list12, SysMenu tlist) {
 
         List<SysMenu> tlist1=new ArrayList<SysMenu>();
         Iterator<SysMenu> it=list12.iterator();
-        while (it.hasNext())
-        {
+        while (it.hasNext()) {
             SysMenu n=(SysMenu) it.next();
 
-            if (n.getParentId().longValue()==tlist.getMenuId().longValue());
+            if (n.getParentId().longValue() == tlist.getMenuId().longValue()) ;
             {
-                tlist1.add(n);
+                tlist1.add( n );
 
             }
         }
@@ -75,9 +66,9 @@ public class TreeUtils {
     }
 
 
-//    判断子节点是不是空
-    private  static boolean hasChild(List<SysMenu> list,SysMenu t){
+    //    判断子节点是不是空
+    private static boolean hasChild(List<SysMenu> list, SysMenu t) {
 
-        return getChildList(list,t).size()>0?true:false;
+        return getChildList( list, t ).size() > 0 ? true : false;
     }
 }
--- src/main/java/com/example/demo/controller/indexController.java
+++ src/main/java/com/example/demo/controller/indexController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tk.mybatis.mapper.annotation.RegisterMapper;
 
 import java.util.List;
 
@@ -43,14 +44,15 @@ private SysMenuService sysMenuService;
         map.addAttribute("msg2","你已经登录成功");
         map.addAttribute("msg1","现在是主界面");
 
-        List<SysMenu> menus=sysMenuService.selectMenunormalall1(user1);
+//        List<SysMenu> menus=sysMenuService.selectMenunormalall1(user1);
 
-        map.put("meuns",menus);
+//        map.put("meuns",menus);
         map.put("user",user1);
+        map.addAttribute( "user1",user1 );
 
-        System.out.println( menus.toString() );
-        map.addAttribute("msg3",sysMenuService.selectMenunormalall1(user1));
-        map.addAttribute("msg4",sysMenuService.selectMenunormalall1(user1));
+//        System.out.println( menus.toString() );
+//        map.addAttribute("msg3",sysMenuService.selectMenunormalall1(user1));
+        map.addAttribute("msg4",sysMenuService.selectMenuall());
 
 //        map.put("copyrightyear")
 
@@ -60,11 +62,46 @@ private SysMenuService sysMenuService;
     }
 
     @RequestMapping("/system/main")
-    public String main(){
+    public String main1(){
 
 
         return "main";
     }
 
+
+    @RequestMapping("index12")
+    public String index12(ModelMap map) {
+
+        SysUser user=null;
+
+        Object obj = getSubject().getPrincipal();
+        System.out.println("shuchu要复制的内容"+ obj.toString() );
+
+        user =new SysUser();
+        /**
+         * 这里beanutils.copyProperties(a,b)是a复制给b
+         * a 是源头，b是被复制放。
+         * */
+        BeanUtils.copyProperties(obj,user);
+        System.out.println("看看复制进入没有"+ user.toString() );
+        SysUser user1=user;
+
+//        SysUser user= (SysUser) SecurityUtils.getPrincipal();
+        System.out.println("  user+++111   "+user1.toString());
+        map.addAttribute("msg2","你已经登录成功");
+        map.addAttribute("msg1","现在是主界面");
+
+
+        map.put("user",user1);
+        map.addAttribute( "user1",user1 );
+
+     map.addAttribute("msg12",sysMenuService.selectMenunormalall1(user1));
+        map.addAttribute("msg12",sysMenuService.selectMenuall());
+
+
+
+
+        return "index12";
+    }
 
 }
